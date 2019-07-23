4f80334c5bd899ae2ab1a814dbaf09fbea679575
Eric Hartmann <NWAlpine@users.noreply.github.com>
2019-07-23 09:28:06 -0700

    LED, Switch and Piezo buzzer

--- main.c
+++ main.c
@@ -23,25 +23,59 @@
 // This #include imports the sample_hardware abstraction from that hardware definition.
 #include <hw/sample_hardware.h>
 
+static int button = -1;
+const struct timespec sleepTime = { 1, 0 };
+
+void buzzerBeep(int buzzerId)
+{
+	GPIO_SetValue(buzzerId, GPIO_Value_High);
+	nanosleep(&sleepTime, NULL);
+	GPIO_SetValue(buzzerId, GPIO_Value_Low);
+}
+
 int main()
 {
 	Log_Debug("Starting CMake Hello World application...\n");
 
-	int fd = GPIO_OpenAsOutput(MT3620_GPIO31, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	// button pin assignment
+	button = GPIO_OpenAsInput(MT3620_GPIO34);
+
+	// buzzer
+	int buzzer = GPIO_OpenAsOutput(MT3620_GPIO4, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+
+	// LED pin assignments
+	int redLED = GPIO_OpenAsOutput(MT3620_GPIO31, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+	int greenLED = GPIO_OpenAsOutput(MT3620_GPIO35, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+
+	/*
 	if (fd < 0) {
 		Log_Debug(
 			"Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
 			strerror(errno), errno);
 		return -1;
 	}
+	*/
 
-	const struct timespec sleepTime = { 1, 0 };
+	GPIO_Value_Type buttonState;
+	GPIO_Value_Type prevButtonState;
+
 	while (true) {
+		/*
 		GPIO_SetValue(fd, GPIO_Value_Low);
 		nanosleep(&sleepTime, NULL);
 		GPIO_SetValue(fd, GPIO_Value_High);
 		nanosleep(&sleepTime, NULL);
-	}
+		*/
 
+		GPIO_GetValue(button, &buttonState);
+		if (!(buttonState == prevButtonState))
+		{
+			GPIO_SetValue(redLED, buttonState);
+			GPIO_SetValue(greenLED, !buttonState);
+			prevButtonState = buttonState;
+
+			buzzerBeep(buzzer);
+		}
+	}
 }
 
